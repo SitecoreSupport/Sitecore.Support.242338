0150ba2bca7b433d320c035f835f4e2a5b281ad0
Paul Stupka <paul.stupka@sitecore.com>
2019-08-02 16:49:49 +0300

    update packages and add base implementation for 1.6

---
+++ src/Sitecore.Support.242338/XA/Foundation/Search/Pipelines/IoC/RegisterSearchServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using Sitecore.XA.Foundation.Search.Services;
+
+namespace Sitecore.Support.XA.Foundation.Search.Pipelines.IoC
+{
+    [UsedImplicitly]
+    public class RegisterSearchServices : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<ISearchService, Sitecore.Support.XA.Foundation.Search.Services.SearchService>();
+        }
+    }
+}
---
+++ src/Sitecore.Support.242338/XA/Foundation/Search/Services/SearchService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Utilities;
+using Sitecore.Support.XA.Foundation.Search.Extensions;
+using Sitecore.XA.Foundation.Search.Models;
+using Sitecore.XA.Foundation.Search.Services;
+using Sitecore.XA.Foundation.SitecoreExtensions.Utils;
+
+namespace Sitecore.Support.XA.Foundation.Search.Services
+{
+    public class SearchService : Sitecore.XA.Foundation.Search.Services.SearchService, ISearchService
+    {
+        IQueryable<ContentPage> ISearchService.GetQuery(string query, string scope, string language, Coordinates center, string siteName, out string indexName)
+        {
+            ISearchIndex searchIndex = IndexResolver.ResolveIndex();
+
+            indexName = searchIndex.Name;
+
+            IEnumerable<SearchStringModel> model = ItemUtils.Lookup(scope, Context.Database).Select(i => i[Sitecore.XA.Foundation.Search.Constants.ScopeQuery]).SelectMany(SearchStringModel.ParseDatasourceString);
+            IQueryable<ContentPage> queryable = LinqHelper.CreateQuery<ContentPage>(searchIndex.CreateSearchContext(), model);
+
+            queryable = queryable.Where(IsGeolocationRequest ? GeolocationPredicate(siteName) : PageOrMediaPredicate(siteName));
+            queryable = queryable.Where(ContentPredicate(query));
+            queryable = queryable.Where(LanguagePredicate(language));
+            queryable = queryable.Where(LatestVersionPredicate());
+            queryable = queryable.ApplyFacetFilters(Context.Request.QueryString, center, siteName);
+            return queryable;
+        }
+
+        protected override IQueryable<ContentPage> GetQuery(string query, string scope, string language, Coordinates center, string siteName)
+        {
+            string indexName;
+            return (this as ISearchService).GetQuery(query, scope, language, center, siteName, out indexName);
+        }
+    }
+}
